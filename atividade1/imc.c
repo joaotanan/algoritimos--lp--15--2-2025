79fee8063bef2ca52f67a5baae9a4aa1377e3a8f
joaotanan2004 <joaogtanan@gmail.com>
2025-04-03 11:53:05 -0300

    Update imc.c

--- atividade1/imc.c
+++ atividade1/imc.c
@@ -17,5 +17,31 @@ int main()
     // Exibindo o resultado
     printf("O seu IMC é: %.2f\n", imc);
 
+    if(imc <= 18.5){
+        printf("baixo peso\n");
+    }
+
+    if(imc > 18.5 && imc <= 24.9){
+        printf("intervalo normal\n");
+    }
+
+    if(imc > 24.9 && imc <= 29.9){
+        printf("sobrepeso\n");
+    }
+
+    if(imc > 29.9 && imc <= 34.9){
+        printf("obesidade I\n");
+    }
+
+    if(imc > 34.9 && imc <= 39.9){
+        printf("obesidade II\n");
+    }
+
+    if(imc > 39.9){
+        printf("obesidade III\n");
+    }
+
+
+
     return 0;
 }
