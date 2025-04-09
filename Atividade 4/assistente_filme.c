0c4dc428394f4a9152cbec753167ad4407252c72
joaotanan2004 <joaogtanan@gmail.com>
2025-04-09 15:09:17 -0300

    Update assistente_filme.c

--- Atividade 4/assistente_filme.c
+++ Atividade 4/assistente_filme.c
@@ -8,13 +8,13 @@ int main() {
     printf("2 - Intenso\n");
     scanf("%d", &tipoFilme);
 
-    if (tipoFilme = 1) {
+    if (tipoFilme == 1) {
         printf("Escolha o genero:\n");
         printf("1 - Comedia\n");
         printf("2 - Animacao\n");
         scanf("%d", &genero);
 
-        if (genero = 1) {
+        if (genero == 1) {
             printf("Assista ao filme: As Branquelas\n");
         } else if (genero == 2) {
             printf("Assista ao filme: Toy Story\n");
@@ -22,35 +22,35 @@ int main() {
             printf("Opcao invalida.\n");
         }
 
-    } else if (tipoFilme = 2) {
+    } else if (tipoFilme == 2) {
         printf("Escolha o genero:\n");
         printf("1 - Terror\n");
         printf("2 - Acao\n");
         scanf("%d", &genero);
 
-        if (genero = 1) {
+        if (genero == 1) {
             printf("Escolha o tipo de terror:\n");
             printf("1 - Psicologico\n");
             printf("2 - Sobrenatural\n");
             scanf("%d", &subtipo);
 
-            if (subtipo = 1) {
+            if (subtipo == 1) {
                 printf("Assista ao filme: Coral\n");
-            } else if (subtipo = 2) {
+            } else if (subtipo == 2) {
                 printf("Assista ao filme: Invocacao do mal\n");
             } else {
                 printf("Opcao invalida.\n");
             }
 
-        } else if (genero = 2) {
+        } else if (genero == 2) {
             printf("Escolha o tipo de acao:\n");
             printf("1 - Super-herois\n");
             printf("2 - Aventura realista\n");
             scanf("%d", &subtipo);
 
-            if (subtipo = 1) {
+            if (subtipo == 1) {
                 printf("Assista ao filme: Vingadores: Ultimato\n");
-            } else if (subtipo = 2) {
+            } else if (subtipo == 2) {
                 printf("Assista ao filme: Mad Max: Estrada da Furia\n");
             } else {
                 printf("Opcao invalida.\n");
