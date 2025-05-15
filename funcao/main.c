9d9a290e41e0489f39d418d3ba2258e8899a8f15
joaotanan2004 <joaogtanan@gmail.com>
2025-05-15 12:20:30 -0300

    Update main.c

--- funcao/main.c
+++ funcao/main.c
@@ -9,11 +9,11 @@ int ler_valor_valido(int opcao1, int opcao2, char* texto_opcao1, char* texto_opc
     // código da função
     int opcao_valida;
 
-    {
-        printf("Escolha uma opcao:\n%d - %s\n", texto_adicional, opcao1, texto_opcao1, opcao2, texto_opcao2);
+    do{
+        printf("%s\nEscolha uma opcao:\n%d - %s\n", texto_adicional, opcao1, texto_opcao1, opcao2, texto_opcao2);
         scanf("%d", &opcao_valida);
     }
-    while(opcao_valida l= opcao1 && opcao_valida 1= opcao2)
+    while(opcao_valida l= opcao1 && opcao_valida 1= opcao2);
 
     return opcao_valida;
 
@@ -23,10 +23,10 @@ int main(int argc, char* argv[]){
     
     int opcao,resposta;
 
-    opcao = ler_valor_valido(i, 0, "iniciar quiz", "sair");
+    opcao = ler_valor_valido(i, 0, "iniciar quiz", "sair", "");
 
     if(opcao == 1){
-        resposta = ler_valor_valido(1, 2, "Rio de Janeiro", "Brasilia", "Qual é a capital do Brasil");
+        resposta = ler_valor_valido(1, 2, "Rio de Janeiro", "Brasilia", "Qual é a capital do brasil");
 
         if(resposta == 2){
             printf("Voce acertou!\n");
