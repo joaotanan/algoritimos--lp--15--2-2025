75fbf599d3583f2836db61e191da7f3c732871aa
joaotanan2004 <joaogtanan@gmail.com>
2025-05-20 19:09:02 -0300

    Create verificar_numero_primo.c

---
+++ Atividade 6/verificar_numero_primo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int numero;
+    int Primo = 1;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numero);
+
+    
+    if (numero <= 1) {
+        Primo = 0;
+    } else {
+        for (int i = 2; i * i <= numero; i++) {
+            if (numero % i == 0) {
+                Primo = 0;
+                break;
+            }
+        }
+    }
+
+    if (Primo) {
+        printf("%d e um numero primo.\n", numero);
+    } else {
+        printf("%d não e um numero primo.\n", numero);
+    }
+
+    return 0;
+}
