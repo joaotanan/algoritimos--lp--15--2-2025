9a2612a4380a9ad64f2ad82f7f1f5aa731ac5f12
joaotanan2004 <joaogtanan@gmail.com>
2025-06-06 11:51:44 -0300

    Create escrever.c

---
+++ ponteiros/escrever.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define MAX_TXT 250
+
+int main(int argc, char* argv[]){
+
+    char texto[MAX_TXT];
+    FILE *arq = fopen(argv[1], "w");
+
+    if(arq == NULL){
+        printf("erro ao abrir arquivo.");
+        return 1;
+    }
+
+    fprintf(arq, argv[2]);
+
+    fclose(arq);
+
+    return 0;
+}
